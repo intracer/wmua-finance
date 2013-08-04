c4a1347fcbf1c72dcb891d2a5b0fbfcb769dda20
Ilya Korniyko <intracer@gmail.com>
2013-08-04 14:49:41 +0300

    extract filtering

--- app/controllers/Operations.scala
+++ app/controllers/Operations.scala
@@ -5,6 +5,7 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.mvc._
 import org.intracer.finance.User
+import org.intracer.finance.Operation
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.DateTime
 import com.github.nscala_time.time.Imports._
@@ -31,45 +32,56 @@ object Operations extends Controller with Secured {
 
         val daterange = map.get("daterange")
 
-        //      val (projects, categories, grants) = form.bindFromRequest.get
+        var operations: Seq[Operation] = filterOperations(projects, categories, grants, daterange)
 
-        var operations = Global.operations.sortBy(_.date.toString()).toSeq
+        Ok(views.html.operations(new User("WM UA"), operations, projects, categories, grants, daterange.map(_.head).getOrElse("")))
+  }
 
-        if (!projects.isEmpty) {
-          operations = operations.filter(op => projects.contains(op.to.projectCode.name))
+
+  def filterOperations(projects: Set[String], categories: Set[String], grants: Set[String], daterange: Option[Seq[String]]): Seq[Operation] = {
+    var operations = Global.operations.sortBy(_.date.toString()).toSeq
+
+    if (!projects.isEmpty) {
+      operations = operations.filter(op => projects.contains(op.to.projectCode.name))
+    }
+    if (!categories.isEmpty) {
+      operations = operations.filter(op => categories.contains(op.to.categoryCode.name))
+    }
+
+    if (!grants.isEmpty) {
+      operations = operations.filter(op => op.to.grantCode.exists(grant => grants.contains(grant.name)))
+    }
+
+    daterange.foreach {
+      range =>
+        val head: String = range.head
+
+        if (!head.trim.isEmpty) {
+          val arr = head.split("-")
+          val pattern = "MM/dd/yyyy";
+
+          val dates = arr.map(entry =>
+            DateTime.parse(entry.trim, DateTimeFormat.forPattern(pattern))
+          )
+
+          operations = operations.filter(op => op.date >= dates(0) && op.date <= dates(1))
         }
-        if (!categories.isEmpty) {
-          operations = operations.filter(op => categories.contains(op.to.categoryCode.name))
-        }
 
-        if (!grants.isEmpty) {
-          operations = operations.filter(op => op.to.grantCode.exists(grant => grants.contains(grant.name)))
-        }
-
-        daterange.foreach {
-          range =>
-            val head: String = range.head
-
-            if (!head.trim.isEmpty) {
-              val arr = head.split("-")
-              val pattern = "MM/dd/yyyy";
-
-              val dates = arr.map(entry =>
-                DateTime.parse(entry.trim, DateTimeFormat.forPattern(pattern))
-              )
-
-              operations = operations.filter(op => op.date >= dates(0) && op.date <= dates(1))
-            }
-
-        }
-
-        Ok(views.html.operations(new User("Illia Korniiko"), operations, projects, categories, grants, daterange.map(_.head).getOrElse("")))
+    }
+    operations
   }
 
   def statistics() = Action {
     implicit request =>
 
-      val operations = Global.operations.sortBy(_.date.toString()).toSeq
+      val map = request.queryString
+      val projects = map.getOrElse("projects", Nil).toSet
+      val categories = map.getOrElse("categories", Nil).toSet
+      val grants = map.getOrElse("grants", Nil).toSet
+
+      val daterange = map.get("daterange")
+
+      var operations: Seq[Operation] = filterOperations(projects, categories, grants, daterange)
 
       val operationsByProject = operations.groupBy(o => o.to.projectCode.name)
       val operationsByCategory = operations.groupBy(o => o.to.categoryCode.name)

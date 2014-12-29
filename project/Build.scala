bae42678b0f898751880192c7ccc572728f7b043
ilya <intracer@gmail.com>
2014-12-30 01:36:52 +0200

    grants + cr/lf

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,4 @@
 import sbt._
-import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -14,7 +12,11 @@ object ApplicationBuild extends Build {
     "org.apache.poi" % "poi" % "3.9",
     "org.apache.poi" % "poi-ooxml" % "3.9",
     "org.scalaz" % "scalaz-core_2.10" % "7.0.0",
-    "com.github.nscala-time" %% "nscala-time" % "0.4.2"
+    "com.github.nscala-time" %% "nscala-time" % "0.4.2",
+    "org.intracer" %% "mwbot" % "0.2.0",
+    "com.squants"  %% "squants"  % "0.4.2",
+    "org.sweble.wikitext" % "swc-engine" % "2.0.0",
+    "com.google.gdata" % "core" % "1.47.1"
   )
 
 

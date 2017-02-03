d13698fb427a058712b287570c91eafbf5f0dc47
ilya <intracer@gmail.com>
2017-02-03 20:20:15 +0200

    simpilfy long test a bit

--- test/org/intracer/finance/slick/DbSpec.scala
+++ test/org/intracer/finance/slick/DbSpec.scala
@@ -132,43 +132,25 @@ class DbSpec extends Specification with InMemDb {
         val accountDao = new AccountDao
         val userDao = new UserDao
 
-        val category1 = CategoryF(name = "cat1")
-        val category2 = CategoryF(name = "cat2")
+        categoryDao.insertAll((1 to 2).map(i => CategoryF(name = "cat" + i)))
+        val cats = categoryDao.list
 
-        categoryDao.insertAll(Seq(category1, category2))
+        projectDao.insertAll((1 to 2).map(i => Project(name = "p" + i)))
+        val projects = projectDao.list
 
-        val cats = categoryDao.list.groupBy(_.id.get).mapValues(_.head)
-        cats.size === 2
+        grantDao.insertAll((1 to 2).map(i => Grant(name = "Grant" + i)))
+        val grants = grantDao.list
 
-        val project1 = Project(name = "p1")
-        val project2 = Project(name = "p2")
-        projectDao.insertAll(Seq(project1, project2))
-
-        val projects = projectDao.list.groupBy(_.id.get).mapValues(_.head)
-        projects.size === 2
-
-        val grant1 = Grant(name = "Grant1")
-        val grant2 = Grant(name = "Grant2")
-        grantDao.insertAll(Seq(grant1, grant2))
-        val grants = grantDao.list.groupBy(_.id.get).mapValues(_.head)
-
-        val grant = grants.values.find(_.name == "Grant1").get
+        val grant = grants.find(_.name == "Grant1").get
         val grantId = grant.id
         val grantItem1 = GrantItem(None, grantId, "1", "item1", BigDecimal.valueOf(100))
         val grantItem2 = GrantItem(None, grantId, "2", "item2", BigDecimal.valueOf(300))
         grantItemDao.insertAll(Seq(grantItem1, grantItem2))
 
-        val grantItems = grantItemDao.list(grantId.get).groupBy(_.id.get).mapValues(_.head)
+        val grantItems = grantItemDao.list(grantId.get)
 
-        val account1 = Account(name = "Account1")
-        val account2 = Account(name = "Account2")
-        accountDao.insertAll(Seq(account1, account2))
-        val accounts = accountDao.list.groupBy(_.id.get).mapValues(_.head)
-
-        //      Expenditures.accounts = accounts
-        //      Expenditures.grants = grants
-        //      Expenditures.categories = cats
-        //      Expenditures.projects = projects
+        accountDao.insertAll((1 to 2).map(i => Account(name = "Account" + i)))
+        val accounts = accountDao.list
 
         val user = User(None, "user", "mail@dot.com")
         val userId = userDao.insert(user)
@@ -177,11 +159,11 @@ class DbSpec extends Specification with InMemDb {
           Some(1),
           new Timestamp(0L),
           Some(BigDecimal("10.00")),
-          accounts.values.find(_.name == "Account1").get,
-          cats.values.find(_.name == "cat1").get,
-          projects.values.find(_.name == "p1").get,
+          accounts.find(_.name == "Account1").get,
+          cats.find(_.name == "cat1").get,
+          projects.find(_.name == "p1").get,
           Some(grant),
-          Some(grantItems.values.head), "exp1",
+          Some(grantItems.head), "exp1",
           user.copy(id = Some(userId))
         )
 

6486905ec8746f2735583b732230127b57fe1012
ilya <intracer@gmail.com>
2017-02-16 19:25:33 +0200

    remove sensitive data

--- app/org/intracer/finance/User.scala
+++ app/org/intracer/finance/User.scala
@@ -64,23 +64,3 @@ object UserObj {
     }
   }
 }
-
-//object User {
-//  def login(username: String, password: String): Option[User] = {
-//    if (sha1(username + "/" + password) == "***REMOVED***") {
-//      Some(new User(***REMOVED***))
-//    } else {
-//      None
-//    }
-//  }
-//
-//  def sha1(input: String) = {
-//
-//    val digest = MessageDigest.getInstance("SHA-1")
-//
-//    digest.update(input.getBytes(), 0, input.length())
-//
-//    new BigInteger(1, digest.digest()).toString(16)
-//  }
-//
-//}

e20621d40cc34a95a37ac924bfdf95dcde2a8fc0
ilya <intracer@gmail.com>
2017-02-20 13:18:11 +0200

    H2 SQL -> MYSQL // TODO fix and make same one

--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -1,18 +1,18 @@
 # --- !Ups
 
-CREATE TABLE "op_id" (
-  "id" SERIAL NOT NULL PRIMARY KEY,
-  "rev_id" INT,
-  FOREIGN KEY ("rev_id") REFERENCES "operation" ("id")
+CREATE TABLE op_id (
+  id SERIAL NOT NULL PRIMARY KEY,
+  rev_id INT,
+  FOREIGN KEY (rev_id) REFERENCES operation(id)
 );
 
-ALTER TABLE "operation" add COLUMN "parent_rev_id" INT;
+ALTER TABLE operation add COLUMN parent_rev_id INT;
 
-ALTER TABLE "operation" ADD COLUMN "op_id" INT NOT NULL;
+ALTER TABLE operation ADD COLUMN op_id INT NOT NULL;
 
-ALTER TABLE "operation"
-  ADD CONSTRAINT "operation_op_id_id_fk"
-FOREIGN KEY ("op_id") REFERENCES "op_id" ("id");
+ALTER TABLE operation
+  ADD CONSTRAINT operation_op_id_id_fk
+FOREIGN KEY (op_id) REFERENCES op_id (id);
 
 UPDATE operation set op_id = id;
 

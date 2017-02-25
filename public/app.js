2dc791a184f86559733873e9eb397e220566bdf2
ilya <intracer@gmail.com>
2017-02-26 00:09:25 +0200

    operations.service

---
+++ public/app.js
@@ -0,0 +1,2 @@
+var financeApp = angular.module("financeApp", ["xeditable", "ui.bootstrap", "ui.select",
+    'mwl.confirm', "ngSanitize", "ngTable", "daterangepicker"]);
---
+++ public/operations.service.js
@@ -0,0 +1,37 @@
+financeApp.service('Operations', [ '$http', function($http) {
+    this.saveOperation = function (data, operation) {
+        if (!moment(data.date).isValid()) {
+            return "Undefined date"
+        }
+        var dateStr = data.date.toISOString().slice(0, 10);
+        if (operation.id == 'new') {
+            operation.id = null
+        }
+        angular.extend(data, {
+            id: operation.id,
+            op_id: operation.op_id,
+            date: dateStr
+        });
+        return $http.post('/newoperation', data);
+    };
+
+    this.loadOperations = function (filter) {
+        if (filter.dateRange.startDate && filter.dateRange.endDate) {
+            var daterange = filter.dateRange.startDate.format("MM/DD/YYYY") + ' - '
+                + filter.dateRange.endDate.format("MM/DD/YYYY")
+        }
+        return $http({
+            url: '/operations_ws',
+            method: "GET",
+            params: {
+                projects: filter.projects.join(),
+                categories: filter.categories.join(),
+                grants: filter.grants.join(),
+                grantItems: filter.grant_items.join(),
+                accounts: filter.accounts.join(),
+                daterange: daterange
+            }
+        })
+    };
+
+}]);

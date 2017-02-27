20a24d7b4f96fde00871b2820dd24acb8065a13c
ilya <intracer@gmail.com>
2017-02-27 09:40:53 +0200

    extract common code

--- app/org/intracer/finance/Dictionary.scala
+++ app/org/intracer/finance/Dictionary.scala
@@ -21,31 +21,39 @@ case class Dictionary(accountMap: Map[Int, Account] = Map.empty,
   def project(id: Int) = projectMap(id)
   def user(id: Int) = userMap(id)
 
-  def accountsJson: String = {
-    accountMap.toSeq.sortBy(_._2.name.toLowerCase).map {
-      case (id, account) => s"""{ value: "$id", text: "${account.name}"}"""
+  def hasNameMapToJson(map: Map[Int, HasName]): String = hasNameSeqToJson(map.values)
+
+  def hasNameSeqToJson(hasNames: Iterable[HasName]): String = {
+    hasNames.toSeq.sortBy(_.name.toLowerCase).map { hasName =>
+      s"""{ value: "${hasName.id}", text: "${hasName.name}"}"""
     }.mkString(", ")
   }
+
+  def hasNameWithParentsToJson(map: Map[String, Seq[HasName]]) = {
+    map.map { case (parent, children) =>
+      s"""{text: "$parent", children: [""" +
+        hasNameSeqToJson(children) +
+        "]}"
+    }.mkString(", ")
+  }
+
+  def accountsJson: String = hasNameMapToJson(accountMap)
+
+  def grantsJson: String =  hasNameMapToJson(grantMap)
+
+  def projectsJson: String =  hasNameMapToJson(projectMap)
 
   def categoriesJson: String = {
     val elems = categoryMap.values.flatMap(_.name.split("/").headOption).toSeq
     val parents = SortedSet(elems: _*)
 
-    parents.map { parent =>
-      s"""{text: "$parent", children: [""" +
-        categoryMap.toSeq
-          .filter(_._2.name.toLowerCase.startsWith(parent.toLowerCase))
-          .sortBy(_._2.name.toLowerCase)
-          .map {
-            case (id, cat) => s"""{ value: "$id", text: "${cat.name}"}"""
-          }.mkString(", ") + "]}"
-    }.mkString(", ")
-  }
-
-  def grantsJson: String = {
-    grantMap.toSeq.sortBy(_._2.name.toLowerCase).map {
-      case (id, grant) => s"""{ value: "$id", text: "${grant.name}"}"""
-    }.mkString(", ")
+    val map = parents.map { parent =>
+      parent ->
+        categoryMap.values.toSeq
+          .filter(_.name.toLowerCase.startsWith(parent.toLowerCase))
+          .sortBy(_.name.toLowerCase)
+    }.toMap
+    hasNameWithParentsToJson(map)
   }
 
   def grantItemsJsonMap: Map[Int, String] = {
@@ -56,24 +64,18 @@ case class Dictionary(accountMap: Map[Int, Account] = Map.empty,
   }
 
   def grantItemsJson(grantId: Int): String = {
-    val programs = Seq(
-      "Program 1: Outreach", "Program 2: Contests", "Program 3: Community Support", "Administrative costs"
-    )
-    (1 to 4).map { program =>
-      s"""{text: "${programs(program - 1)}", children: [""" +
+    val parents = Seq("Program 1: Outreach",
+      "Program 2: Contests",
+      "Program 3: Community Support",
+      "Administrative costs")
+
+    val map = parents.zipWithIndex.map { case (parent, index) =>
+      parent ->
         grantItemMap
           .getOrElse(grantId, Seq.empty)
-          .filter(_.number.startsWith(program.toString))
-          .map {
-            item => s"""{ value: "${item.id.get}", text: "${item.name}"}"""
-          }.mkString(", ") + "]}"
-    }.mkString(", ")
-  }
-
-  def projectsJson: String = {
-    projectMap.toSeq.sortBy(_._2.name.toLowerCase).map {
-      case (id, project) => s"""{ value: "$id", text: "${project.name}"}"""
-    }.mkString(", ")
+          .filter(_.number.startsWith((index + 1).toString))
+    }.toMap
+    hasNameWithParentsToJson(map)
   }
 }
 

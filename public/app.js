a6549922027523e8fceddaa8b553e139d244f892
ilya <intracer@gmail.com>
2017-03-05 13:51:03 +0200

    angular: use ui-router

--- public/app.js
+++ public/app.js
@@ -1,25 +1,31 @@
 var financeApp = angular.module("financeApp",
     ["xeditable", "ui.bootstrap", "ui.select",
-        'mwl.confirm', "ngSanitize", "ngTable", "daterangepicker", 'ngRoute']).config(['$locationProvider', '$routeProvider',
-    function config($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
+        'mwl.confirm', "ngSanitize", "ngTable", "daterangepicker", 'ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider',
+        function config($stateProvider, $urlRouterProvider) {
 
-        $routeProvider
-            .when('/operations', {
-                templateUrl: 'operations.template.html'
-            })
-            .when('/grouped', {
-                templateUrl: 'grouped.template.html'
-            })
-            .when('/accounts', {
-                templateUrl: 'dictionary.template.html'
-            })
-            .when('/categories', {
-                templateUrl: 'dictionary.template.html'
-            })
-            .when('/projects', {
-                templateUrl: 'dictionary.template.html'
-            })
-            .otherwise('/operations');
-    }
-]);
+            $urlRouterProvider.otherwise('/operations');
+
+            $stateProvider
+                .state('operations', {
+                    url: '/operations',
+                    templateUrl: 'operations.template.html'
+                })
+                .state('grouped', {
+                    url: '/grouped',
+                    templateUrl: 'grouped.template.html'
+                })
+                .state('accounts', {
+                    url: '/accounts',
+                    templateUrl: 'dictionary.template.html'
+                })
+                .state('categories', {
+                    url: '/categories',
+                    templateUrl: 'dictionary.template.html'
+                })
+                .state('projects', {
+                    url: '/projects',
+                    templateUrl: 'dictionary.template.html'
+                })
+        }
+    ]);
